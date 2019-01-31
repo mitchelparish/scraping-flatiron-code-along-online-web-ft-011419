a28ae4c5b540e6cd92a77aa636146a3928e45919
Mitchel Parish <mitchelparish@gmail.com>
2019-01-31 19:53:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,18 +8,10 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    binding.pry 
+    binding.pry
   end
 
-
-
-
-
-
-
-
-
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -30,3 +22,5 @@ class Scraper
     end
   end
 end
+
+Scraper.new.get_page
